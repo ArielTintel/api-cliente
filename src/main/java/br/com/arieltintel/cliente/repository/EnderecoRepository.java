3118ddac1b39b726911f7895138c9cda0a48b5a0
Ariel Andrade Tintel <arielandrade97@gmail.com>
2022-05-11 23:23:10 -0300

    Adicionado Servico de consultar Endereco por Email

--- src/main/java/br/com/arieltintel/cliente/repository/EnderecoRepository.java
+++ src/main/java/br/com/arieltintel/cliente/repository/EnderecoRepository.java
@@ -1,2 +1,16 @@
-package br.com.arieltintel.cliente.repository;public interface EnderecoRepository {
+package br.com.arieltintel.cliente.repository;
+
+import br.com.arieltintel.cliente.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+    @Query("""
+            FROM Endereco ec
+            JOIN Cliente c ON ec.id = c.id
+            WHERE c.email = :email
+            """)
+    Endereco findByClienteEmail(String email);
+
 }
